51e8856d356e8a680d37a201cafdea5b9e25db5c
Frédéric Delorme <frederic.delorme@gmail.com>
2021-05-16 23:41:18 +0200

    Create fr.snapgames.fromclasstogame.GameBDDTests.java

---
+++ src/test/java/fr.snapgames.fromclasstogame.GameBDDTests.java
@@ -0,0 +1,18 @@
+package fr.snapgames.fromclasstogame;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * TDD entry point for the test execution
+ *
+ * @author Frédéric Delorme
+ * @since 0.0.1
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue = { "features" }, plugin = {
+        "html:target/cucumber-html-report", "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml", })
+class GameBDDTests{
+}
