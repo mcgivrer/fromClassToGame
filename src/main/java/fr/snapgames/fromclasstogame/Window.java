5afd56ab06af2afbeb1296cee866ae8a2e0442ae
Frédéric Delorme <frederic.delorme@gmail.com>
2021-07-29 19:07:48 +0200

    feat(addWindow): Add a Window object
    - Create Wndow object
    - Update Game to support Window.

---
+++ src/main/java/fr/snapgames/fromclasstogame/Window.java
@@ -0,0 +1,29 @@
+package fr.snapgames.fromclasstogame;
+
+import javax.swing.JFrame;
+
+import java.awt.GraphicsDevice;
+import java.awt.Dimension;
+import java.awt.Point;
+
+public class Window {
+
+    private JFrame frame;
+
+    public Window(String title, int width, int height) {
+        frame = new JFrame(title);
+
+        GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+        Dimension dim = new Dimension(width, height);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(dim);
+        frame.setPreferredSize(dim);
+        frame.setMaximumSize(dim);
+        frame.setLocation(new Point((int) (device.getDisplayMode().getWidth() - dim.width) / 2,
+                (int) (device.getDisplayMode().getHeight() - dim.height) / 2));
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
