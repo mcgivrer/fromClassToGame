d0d73a5ba13f742f958d7dfa8cd56ec542032e74
Frédéric Delorme <frederic.delorme@gmail.com>
2021-08-20 00:53:34 +0200

    feat(CameraUpdate): Add new feature to Camera
    - Refactor Camera,
    - Refactor Rendering of GameObject (by adding GOType) and managing Layer and Camera fixed object,
    - adapt DemoScene to new Camera and Rendering capabilities.

--- src/main/java/fr/snapgames/fromclasstogame/demo/scenes/DemoScene.java
+++ src/main/java/fr/snapgames/fromclasstogame/demo/scenes/DemoScene.java
@@ -37,24 +37,31 @@ public class DemoScene extends AbstractScene {
     @Override
     public void create(Game g) throws UnknownResource {
         // add main character (player)
-        GameObject player = new GameObject("player", 160, 100).setColor(Color.RED).setSpeed(0.02, 0.02)
-                .setSize(16.0, 16.0).setImage(ResourceManager.getImage("images/tiles.png:player"));
+        GameObject player = new GameObject("player", 160, 100)
+                .setType(GameObject.GOType.IMAGE)
+                .setColor(Color.RED)
+                .setSpeed(0.02, 0.02)
+                .setSize(16.0, 16.0)
+                .setImage(ResourceManager.getImage("images/tiles.png:player"));
         add(player);
 
         Dimension vp = new Dimension(g.getRender().getBuffer().getWidth(),g.getRender().getBuffer().getHeight());
-        Camera camera = new Camera("cam01").setTarget(player).setTweenFactor(0.05).setViewport( vp );
+        Camera camera = new Camera("cam01").setTarget(player).setTweenFactor(0.002).setViewport( vp );
         add(camera);
 
         // Add enemies(enemy_99)
         for (int i = 0; i < 10; i++) {
             GameObject e = new GameObject("enemy_" + i, rand(0, 320), rand(0, 200))
-                    .setSpeed(rand(-0.05, 0.05), rand(-0.05, 0.05)).setColor(Color.ORANGE).setSize(8, 8)
+                    .setType(GameObject.GOType.IMAGE)
+                    .setSpeed(rand(-0.05, 0.05), rand(-0.05, 0.05))
+                    .setColor(Color.ORANGE)
+                    .setSize(8, 8)
                     .setImage(ResourceManager.getImage("images/tiles.png:orangeBall"));
             add(e);
         }
         Font f = ResourceManager.getFont("fonts/FreePixel.ttf").deriveFont(Font.CENTER_BASELINE, 14);
         // add some fixed text.
-        ScoreObject scoreTO = (ScoreObject) new ScoreObject("score", 10, 20).setScore(score).setFont(f);
+        ScoreObject scoreTO = (ScoreObject) new ScoreObject("score", 10, 20).setScore(score).setFont(f).relativeToCamera(true).setLayer(1);
         scoreTO.setColor(Color.WHITE);
         scoreTO.priority = 10;
         add(scoreTO);
