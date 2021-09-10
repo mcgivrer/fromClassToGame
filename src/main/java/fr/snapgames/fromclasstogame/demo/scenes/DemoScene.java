f847c770f05a81f6e3580c149190c16afcb8c7ac
Frédéric Delorme <frederic.delorme@gmail.com>
2021-09-10 22:41:31 +0200

    feat(Life): Add Life display
    - Add `TextValueObject` and its `*RenderHelper`.
    - update `DemoScene` to manage life display as a `TextValueObject`

--- src/main/java/fr/snapgames/fromclasstogame/demo/scenes/DemoScene.java
+++ src/main/java/fr/snapgames/fromclasstogame/demo/scenes/DemoScene.java
@@ -10,7 +10,9 @@ import fr.snapgames.fromclasstogame.core.physic.Material.DefaultMaterial;
 import fr.snapgames.fromclasstogame.core.physic.World;
 import fr.snapgames.fromclasstogame.core.scenes.AbstractScene;
 import fr.snapgames.fromclasstogame.demo.entity.ScoreObject;
+import fr.snapgames.fromclasstogame.demo.entity.TextValueObject;
 import fr.snapgames.fromclasstogame.demo.render.ScoreRenderHelper;
+import fr.snapgames.fromclasstogame.demo.render.TextValueRenderHelper;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -18,6 +20,7 @@ import java.awt.event.KeyEvent;
 public class DemoScene extends AbstractScene {
 
     private int score = 0;
+    private int life = 5;
 
     public DemoScene(Game g) {
         super(g);
@@ -33,9 +36,12 @@ public class DemoScene extends AbstractScene {
         super.initialize(g);
         // Add a specific Render for the new ScoreObject
         g.getRender().addRenderHelper(new ScoreRenderHelper());
+        g.getRender().addRenderHelper(new TextValueRenderHelper());
 
         // load resources
         ResourceManager.getFont("fonts/FreePixel.ttf");
+        ResourceManager.getSlicedImage("images/tiles01.png", "heart", 0, 16, 16, 16);
+        ResourceManager.getSlicedImage("images/tiles01.png", "*", 0, 0, 16, 16);
         ResourceManager.getSlicedImage("images/tiles.png", "player", 0, 0, 16, 16);
         ResourceManager.getSlicedImage("images/tiles.png", "orangeBall", 16, 0, 16, 16);
     }
@@ -55,6 +61,7 @@ public class DemoScene extends AbstractScene {
         Camera camera = new Camera("cam01").setTarget(player).setTweenFactor(0.02).setViewport(vp);
         add(camera);
 
+
         // Add enemies(enemy_99)
         for (int i = 0; i < 10; i++) {
             GameObject e = new GameObject("enemy_" + i, rand(0, 320), rand(0, 200))
@@ -64,21 +71,51 @@ public class DemoScene extends AbstractScene {
                     .setMaterial(DefaultMaterial.RUBBER.getMaterial());
             add(e);
         }
-        Font f = ResourceManager.getFont("fonts/FreePixel.ttf").deriveFont(Font.CENTER_BASELINE, 14);
-        // add some fixed text.
-        ScoreObject scoreTO = (ScoreObject) new ScoreObject("score", 10, 20).setScore(score).setFont(f).relativeToCamera(true).setLayer(1);
+        Font f = ResourceManager.getFont("fonts/FreePixel.ttf").deriveFont(Font.BOLD, 14);
+
+        // add score display.
+        ScoreObject scoreTO = (ScoreObject) new ScoreObject("score", 10, 20)
+                .setScore(score)
+                .setFont(f)
+                .relativeToCamera(true).setLayer(1);
         scoreTO.setColor(Color.WHITE);
         scoreTO.priority = 10;
         add(scoreTO);
+
+        GameObject heart = new GameObject("heart", 280, 10)
+                .setType(GameObject.GOType.IMAGE)
+                .setImage(ResourceManager.getImage("images/tiles01.png:heart"))
+                .relativeToCamera(true).setLayer(1);
+        heart.mass = 0;
+        heart.priority = 10;
+        add(heart);
+        GameObject star = new GameObject("*", 288, 20)
+                .setType(GameObject.GOType.IMAGE)
+                .setImage(ResourceManager.getImage("images/tiles01.png:*"))
+                .relativeToCamera(true);
+        star.mass = 0;
+        star.priority = 10;
+        add(star);
+
+        // add Life counter text.
+        TextValueObject lifeTO = (TextValueObject) new TextValueObject("life", 292, 24)
+                .setValue(life)
+                .setFont(f)
+                .relativeToCamera(true).setLayer(1);
+        lifeTO.setColor(Color.WHITE);
+        lifeTO.priority = 12;
+        add(lifeTO);
     }
 
     @Override
     public void activate() {
         //objects.get("player").setSpeed(0.02, 0.02).setPosition(160, 100);
-        find("enemy_").forEach(go -> {
-            go.setPosition(rand(0, game.getPhysicEngine().getWorld().width), rand(0, game.getPhysicEngine().getWorld().height));
-            go.setSpeed(rand(-0.1, 0.1), rand(-0.1, 0.1));
-        });
+        find("enemy_").forEach(go ->
+                go.setPosition(
+                                rand(0, game.getPhysicEngine().getWorld().width),
+                                rand(0, game.getPhysicEngine().getWorld().height))
+                        .setSpeed(rand(-0.1, 0.1), rand(-0.1, 0.1))
+        );
         this.score = 0;
     }
 
@@ -103,6 +140,8 @@ public class DemoScene extends AbstractScene {
         double speed = 0.0;
         if (inputHandler.getKey(KeyEvent.VK_CONTROL)) {
             speed = 0.8;
+        } else if (inputHandler.getKey(KeyEvent.VK_SHIFT)) {
+            speed = 0.4;
         } else {
             speed = 0.2;
         }
