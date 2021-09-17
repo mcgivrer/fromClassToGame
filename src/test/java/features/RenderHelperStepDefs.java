a362eb258a2718040cbbf934de4e34d06fc7058d
Frédéric Delorme <frederic.delorme@gmail.com>
2021-09-17 18:17:23 +0200

    Try fixing BDD issues on systems

--- src/test/java/features/RenderHelperStepDefs.java
+++ src/test/java/features/RenderHelperStepDefs.java
@@ -1,22 +1,21 @@
 package features;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-
 import fr.snapgames.fromclasstogame.core.Game;
 import fr.snapgames.fromclasstogame.core.exceptions.cli.UnknownArgumentException;
 import fr.snapgames.fromclasstogame.core.gfx.Render;
 import fr.snapgames.fromclasstogame.core.gfx.RenderHelper;
+import fr.snapgames.fromclasstogame.core.scenes.Scene;
+import fr.snapgames.fromclasstogame.core.scenes.SceneManager;
 import fr.snapgames.fromclasstogame.core.system.SystemManager;
 import fr.snapgames.fromclasstogame.test.entity.TestObject;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class RenderHelperStepDefs {
 
@@ -40,7 +39,8 @@ public class RenderHelperStepDefs {
 
     @And("the RenderHelper for {string} is ready")
     public void theRenderHelperForIsReady(String objectName) {
-        Map<String, RenderHelper> renderHelpers = game.getRender().getRenderHelpers();
+        Render render = (Render) SystemManager.get(Render.class);
+        Map<String, RenderHelper> renderHelpers = render.getRenderHelpers();
         RenderHelper rh = renderHelpers.get(objectName);
         assertEquals("The '" + objectName + "' RenderHelper is not defined", objectName, rh.getType());
     }
@@ -60,14 +60,16 @@ public class RenderHelperStepDefs {
     @And("the TestObject named {string} is rendered.")
     public void theTestObjectNamedIsRendered(String objectName) {
 
-        Render render = (Render)SystemManager.get(Render.class);
+        Render render = (Render) SystemManager.get(Render.class);
+        Scene sc = ((SceneManager) SystemManager.get(SceneManager.class)).getCurrent();
         render.render();
-        TestObject to = (TestObject) game.getSceneManager().getCurrent().getGameObject("test");
+        TestObject to = (TestObject) sc.getGameObject("test");
         assertTrue("The TestObject " + objectName + " has not been rendered", to.getFlag());
     }
 
     @And("I add a {string} as scene")
     public void iAddATestScene(String sceneName) {
-        game.getSceneManager().addScene(sceneName);
+        SceneManager sm = (SceneManager) SystemManager.get(SceneManager.class);
+        sm.addScene(sceneName);
     }
 }
