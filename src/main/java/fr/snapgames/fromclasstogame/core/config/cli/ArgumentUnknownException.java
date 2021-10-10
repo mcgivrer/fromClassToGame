c00415251d86a716258251e5508322054cbb8cd4
Frédéric Delorme <frederic.delorme@gmail.com>
2021-10-10 23:37:26 +0200

    feat(upgradeConf): Upgrade the configuration
    - add argument cli parser
    - change configruation to take ArgParser feature.
    
    fix #59

---
+++ src/main/java/fr/snapgames/fromclasstogame/core/config/cli/ArgumentUnknownException.java
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Frédéric Delorme
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package fr.snapgames.fromclasstogame.core.config.cli;
+
+public class ArgumentUnknownException extends Exception {
+
+    public ArgumentUnknownException(String string, String key) {
+        super(String.format(string, key));
+    }
+
+    private static final long serialVersionUID = 1L;
+
+}
