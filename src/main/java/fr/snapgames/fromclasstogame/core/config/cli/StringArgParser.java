7e487f00ec39883d4e4f4c78b07001a8ce6ccdbb
Frédéric Delorme <frederic.delorme@gmail.com>
2021-10-13 20:04:57 +0200

    Merge remote-tracking branch 'origin/feature/upgrade-config-def' into feature/upgrade-config-def

---
+++ src/main/java/fr/snapgames/fromclasstogame/core/config/cli/StringArgParser.java
@@ -0,0 +1,36 @@
+package fr.snapgames.fromclasstogame.core.config.cli;
+
+import fr.snapgames.fromclasstogame.core.config.cli.AbstractArgParser;
+
+import java.util.Optional;
+
+public class StringArgParser extends AbstractArgParser<String> {
+
+    public StringArgParser(String name, String description, String shortKey, String longKey, String configKey, String defaultValue) {
+        super(name, description, shortKey, longKey, configKey);
+        setDefaultValue(defaultValue);
+    }
+
+    @Override
+    public void parseFromConfigFile(String line) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public boolean validate(String strValue) {
+        Optional<String> valueMightBeNull = Optional.of(strValue);
+        if (valueMightBeNull.isPresent()) {
+            this.value = valueMightBeNull.get();
+        } else {
+            this.value = defaultValue;
+        }
+        return true;
+    }
+
+    @Override
+    public String parse(String strValue) {
+        return strValue;
+    }
+}
