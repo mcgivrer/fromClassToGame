a3d4726fd485808fd2846f7f9bc0f3b57ec31c03
Frédéric Delorme <frederic.delorme@gmail.com>
2021-10-18 00:35:36 +0200

    feat(AddLife): Add LifeObject
    - Add a LifeObject  to display live number.
    
    fix #62

---
+++ src/main/java/fr/snapgames/fromclasstogame/demo/render/LifeRenderHelper.java
@@ -0,0 +1,80 @@
+package fr.snapgames.fromclasstogame.demo.render;
+
+import fr.snapgames.fromclasstogame.core.entity.GameObject;
+import fr.snapgames.fromclasstogame.core.entity.TextObject;
+import fr.snapgames.fromclasstogame.core.exceptions.io.UnknownResource;
+import fr.snapgames.fromclasstogame.core.gfx.renderer.RenderHelper;
+import fr.snapgames.fromclasstogame.core.io.ResourceManager;
+import fr.snapgames.fromclasstogame.demo.entity.LifeObject;
+import fr.snapgames.fromclasstogame.demo.entity.ScoreObject;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.charset.StandardCharsets;
+
+public class LifeRenderHelper implements RenderHelper {
+
+    BufferedImage figs[];
+    BufferedImage heart;
+    BufferedImage cross;
+
+    public LifeRenderHelper() {
+        prepareFigures();
+    }
+
+    private void prepareFigures() {
+        try {
+            heart = ResourceManager.getImage("images/tiles01.png:heart");
+            cross = ResourceManager.getImage("images/tiles01.png:*");
+            figs = new BufferedImage[10];
+            for (int i = 0; i < 10; i++) {
+                figs[i] = ResourceManager.getSlicedImage("images/tiles01.png",
+                        "" + i, i * 8, 3 * 16, 8, 16);
+            }
+        } catch (UnknownResource e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics2D g, Object o) {
+        double maxBorderWidth = 2;
+        LifeObject so = (LifeObject) o;
+        g.setColor(so.color);
+        drawLife(g, so.value, (int) (so.position.x), (int) (so.position.y));
+    }
+
+    /**
+     * Draw score with digital characters
+     *
+     * @param g
+     * @param value
+     * @param x
+     * @param y
+     */
+    private void drawLife(Graphics2D g, int value, int x, int y) {
+        g.drawImage(heart, x, y, null);
+        g.drawImage(cross, x + 8, y + 10, null);
+        drawFig(g, value, x + 12, y );
+    }
+
+    /**
+     * Draw a simple figure
+     *
+     * @param g
+     * @param value
+     * @param x
+     * @param y
+     */
+    private void drawFig(Graphics2D g, int value, int x, int y) {
+        assert (value > -1);
+        assert (value < 10);
+        g.drawImage(figs[value], x, y, null);
+    }
+
+    @Override
+    public String getType() {
+        return LifeObject.class.getName();
+    }
+
+}
