587ffb9e6c27968d488dbc4c6d4cb4597c6e6f9d
Frédéric Delorme <frederic.delorme@gmail.com>
2021-10-18 00:35:36 +0200

    feat(AddLife): Add LifeObject
    - Add a LifeObject  to display live number.
    - Change the way to display score by displaying image for all figures.
    
    fix #62

--- src/main/java/fr/snapgames/fromclasstogame/demo/render/ScoreRenderHelper.java
+++ src/main/java/fr/snapgames/fromclasstogame/demo/render/ScoreRenderHelper.java
@@ -1,22 +1,41 @@
 package fr.snapgames.fromclasstogame.demo.render;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import fr.snapgames.fromclasstogame.core.entity.TextObject;
+import fr.snapgames.fromclasstogame.core.exceptions.io.UnknownResource;
 import fr.snapgames.fromclasstogame.core.gfx.renderer.RenderHelper;
+import fr.snapgames.fromclasstogame.core.io.ResourceManager;
 import fr.snapgames.fromclasstogame.demo.entity.ScoreObject;
 
+import javax.imageio.ImageIO;
+
 public class ScoreRenderHelper implements RenderHelper {
+
+    BufferedImage figuresImg;
+    BufferedImage figs[];
+
+    public ScoreRenderHelper() {
+        prepareFigures();
+    }
+
+    private void prepareFigures() {
+        figs = new BufferedImage[10];
+        for (int i = 0; i < 10; i++) {
+            figs[i] = ResourceManager.getSlicedImage("images/tiles01.png",
+                    "" + i, i * 8, 3 * 16, 8, 16);
+        }
+    }
 
     @Override
     public void draw(Graphics2D g, Object o) {
         double maxBorderWidth = 2;
         ScoreObject so = (ScoreObject) o;
         g.setColor(so.color);
-        g.setFont(so.font);
-        drawBorder(g, maxBorderWidth, so);
-        g.setColor(so.color);
-        g.drawString(so.text, (int) (so.position.x), (int) (so.position.y));
+        drawScore(g, so.text, (int) (so.position.x), (int) (so.position.y));
     }
 
     protected void drawBorder(Graphics2D g, double maxBorderWidth, TextObject so) {
@@ -27,6 +46,37 @@ public class ScoreRenderHelper implements RenderHelper {
                 g.drawString(so.text, (int) (x), (int) (y));
             }
         }
+    }
+
+    /**
+     * Draw score with digital characters
+     *
+     * @param g
+     * @param score
+     * @param x
+     * @param y
+     */
+    private void drawScore(Graphics2D g, String score, int x, int y) {
+        byte c[] = score.getBytes(StandardCharsets.US_ASCII);
+        ;
+        for (int pos = 0; pos < score.length(); pos++) {
+            int v = c[pos];
+            drawFig(g, v - 48, x + (pos * 8), y);
+        }
+    }
+
+    /**
+     * Draw a simple figure
+     *
+     * @param g
+     * @param value
+     * @param x
+     * @param y
+     */
+    private void drawFig(Graphics2D g, int value, int x, int y) {
+        assert (value > -1);
+        assert (value < 10);
+        g.drawImage(figs[value], x, y, null);
     }
 
     @Override
