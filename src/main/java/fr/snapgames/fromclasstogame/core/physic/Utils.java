c1e8b1972060297aab8ae6d9b4d17076224dc651
Frédéric Delorme <frederic.delorme@gmail.com>
2021-11-23 01:58:22 +0100

    feat(ParticleFire): Add FireParticleBehavior and RainParticleBehavior
    - Start implementing Fire animation
    - Start implementing  Rain animation
    
    fix #79
    
    =

--- src/main/java/fr/snapgames/fromclasstogame/core/physic/Utils.java
+++ src/main/java/fr/snapgames/fromclasstogame/core/physic/Utils.java
@@ -1,5 +1,7 @@
 package fr.snapgames.fromclasstogame.core.physic;
 
+import java.awt.*;
+
 public class Utils {
 
     public static double rand(double min, double max) {
@@ -18,4 +20,28 @@ public class Utils {
         }
         return r;
     }
+
+    public static Color randomColor(Color c1, Color c2) {
+        int rMax = Math.max(c1.getRed(), c2.getRed());
+        int gMax = Math.max(c1.getGreen(), c2.getGreen());
+        int bMax = Math.max(c1.getBlue(), c2.getBlue());
+        int aMax = Math.max(c1.getAlpha(), c2.getAlpha());
+
+        int rMin = Math.min(c1.getRed(), c2.getRed());
+        int gMin = Math.min(c1.getGreen(), c2.getGreen());
+        int bMin = Math.min(c1.getBlue(), c2.getBlue());
+        int aMin = Math.min(c1.getAlpha(), c2.getAlpha());
+
+        float r = (float) range((Math.random() * rMax + rMin), 0.0, 1.0);
+        float g = (float) range((Math.random() * gMax + gMin), 0.0, 1.0);
+        float b = (float) range((Math.random() * bMax + bMin), 0.0, 1.0);
+        float a = (float) range((Math.random() * aMax + aMin), 0.0, 1.0);
+
+        return new Color(r, g, b, a);
+    }
+
+    public static double range(double c, double min, double max) {
+        return Math.max(min, Math.min(c, max));
+    }
+
 }
