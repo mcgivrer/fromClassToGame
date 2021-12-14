96f97d592655d8335ceeae4817aa0d8186002062
Frédéric Delorme <frederic.delorme@gmail.com>
2021-12-14 01:42:13 +0100

    feat(doc): Add doc on child and rendering
    - update Render to render child objects,
    - update PhysicEngine to update child object,
    - add documentation on GameObject child evolution,
    - add ParticleSystem enhancement to take benefits from child,
    - add documentation on debug info and its rendering (partial).

--- src/main/java/fr/snapgames/fromclasstogame/core/behaviors/DebugSwitcherBehavior.java
+++ src/main/java/fr/snapgames/fromclasstogame/core/behaviors/DebugSwitcherBehavior.java
@@ -36,7 +36,7 @@ public class DebugSwitcherBehavior implements Behavior<Scene> {
         if (objIdx < 0) {
             objIdx = scene.getObjectsList().size() - 1;
         }
-        if (objIdx > scene.getObjectsList().size()) {
+        if (objIdx > scene.getObjectsList().size() - 1) {
             objIdx = 0;
         }
 
