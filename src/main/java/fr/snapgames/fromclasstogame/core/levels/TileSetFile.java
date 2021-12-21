f75421917b0b5c3ea3a4ee4961dc628e8024f87a
Frédéric Delorme <frederic.delorme@gmail.com>
2021-12-21 23:28:30 +0100

    start implmenting the LevelLoader

---
+++ src/main/java/fr/snapgames/fromclasstogame/core/levels/TileSetFile.java
@@ -0,0 +1,10 @@
+package fr.snapgames.fromclasstogame.core.levels;
+
+import fr.snapgames.fromclasstogame.core.entity.tilemap.Tile;
+
+import java.util.Map;
+
+public class TileSetFile {
+    public String imageFile;
+    public Map<String, Tile> tiles;
+}
