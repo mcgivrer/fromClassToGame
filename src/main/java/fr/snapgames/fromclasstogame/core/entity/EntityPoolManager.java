960fe180b98a8d9f986c397d58dd0e53c8b8a3db
Frédéric Delorme <frederic.delorme@gmail.com>
2022-01-20 00:59:01 +0100

    feat(EPM): Add EntityPoolManager
    - add EntityPoolManager
    - add EntityPool
    - update Game to add EPM system.
    - add corresponding tests
    
    TODO: update the AbstractScene to use the corresponding EntityPool.
    
    fix #137

--- src/main/java/fr/snapgames/fromclasstogame/core/entity/EntityPoolManager.java
+++ src/main/java/fr/snapgames/fromclasstogame/core/entity/EntityPoolManager.java
@@ -7,22 +7,76 @@ import fr.snapgames.fromclasstogame.core.system.System;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The pool manager maintains a list of EntityPool to store GameObject.
+ *
+ * @author Frédéric Delorme
+ * @since 0.0.2
+ */
 public class EntityPoolManager extends System {
 
+    /**
+     * Map of Pool
+     */
     private Map<String, EntityPool> entityPools = new HashMap<>();
 
+    /**
+     * Build a pool manager
+     *
+     * @param g
+     */
     public EntityPoolManager(Game g) {
         super(g);
     }
 
+    /**
+     * Add a Pool of Entity.
+     *
+     * @param ep
+     */
     public void addPool(EntityPool ep) {
         this.entityPools.put(ep.getName(), ep);
     }
 
+    /**
+     * Retrieve the EntityPool named entityPoolName.
+     *
+     * @param entityPoolName the name of the entity pool to be retrieved.
+     * @return the corresponding entity pool.
+     */
     public EntityPool get(String entityPoolName) {
         assert (this.entityPools.containsKey(entityPoolName));
         return this.entityPools.get(entityPoolName);
     }
+
+    /**
+     * Retrieve the name of the service.
+     *
+     * @return the name of this System.
+     */
+    public void removePool(String entityPoolName) {
+        this.entityPools.remove(entityPoolName);
+    }
+
+    /**
+     * Create an EntityPool
+     *
+     * @param name the name for this new EntityPool.
+     */
+    public void createPool(String name) {
+        addPool(new EntityPool(name, 1000));
+    }
+
+    /**
+     * Retrieve the EntityPool on its name.
+     *
+     * @param name the name of the EntityPool to retrieve.
+     * @return The corresponding EntityPool.
+     */
+    public EntityPool getPool(String name) {
+        return entityPools.get(name);
+    }
+
 
     @Override
     public String getName() {
@@ -39,15 +93,4 @@ public class EntityPoolManager extends System {
         entityPools.clear();
     }
 
-    public void removePool(String entityPoolName) {
-        this.entityPools.remove(entityPoolName);
-    }
-
-    public void createPool(String name) {
-        addPool(new EntityPool(name, 1000));
-    }
-
-    public EntityPool getPool(String name) {
-        return entityPools.get(name);
-    }
 }
